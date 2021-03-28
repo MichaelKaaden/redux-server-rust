5a82f36c203becc230cfce79806a8302869c81bf
Michael Kaaden <github@kaaden.net>
2021-03-28 22:34:38 +0200

    Create a simple web server returning the app's version and an index page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(version).service(
+            // prefixes all resources and routes attached to it...
+            web::scope("/counters")
+                // ...so this handles requests for "GET /app/index.html"
+                .route("/index.html", web::get().to(index)),
+        )
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
+}
+
+async fn index() -> impl Responder {
+    "Hello world"
+}
+
+#[get("/version")]
+async fn version() -> impl Responder {
+    format!("v{}", built_info::PKG_VERSION)
+}
+
+// Use of a mod or pub mod is not actually necessary.
+pub mod built_info {
+    // The file has been placed there by the build script.
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
